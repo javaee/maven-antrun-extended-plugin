5f970d54cc9d12c50828be42c08074a9989b9f7e
Kohsuke Kawaguchi <kohsuke@java.net>
2007-10-09 18:16:47 +0000

    added the ant task skeleton.
    svn path=/trunk/; revision=12

---
+++ src/main/java/org/apache/maven/plugin/antrun/ResolveArtifactTask.java
@@ -0,0 +1,46 @@
+package org.apache.maven.plugin.antrun;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
+import org.apache.maven.artifact.Artifact;
+
+/**
+ * Ant task that resolves an artifact through Maven.
+ *
+ * TODO: implement the actual resolution logic
+ * TODO: support more convenient syntax
+ * TODO: resolve dependency transitively.
+ * 
+ * @author Paul Sterk
+ * @author Kohsuke Kawaguchi
+ */
+public class ResolveArtifactTask extends Task {
+    private String groupId,artifactId,version,classifier;
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public void setClassifier(String classifier) {
+        this.classifier = classifier;
+    }
+
+    public void execute() throws BuildException {
+        log("Log messages this way", Project.MSG_INFO);
+
+        ArtifactResolverWrapper w = ArtifactResolverWrapperThreadLocal.get();
+
+        // TODO: do the resolution
+        Artifact a = w.getFactory().createArtifactWithClassifier(groupId, artifactId, version, null, classifier);
+        w.getResolver().resolve(...);
+    }
+}
