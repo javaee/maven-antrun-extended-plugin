7ba13ead0775c5ad3bb4e5ea72a8c306295054cc
Paul Sterk <psterk@java.net>
2007-10-09 22:59:53 +0000

    Completed the artifact resolution and handled exceptions.
    svn path=/trunk/; revision=21

--- src/main/java/org/apache/maven/plugin/antrun/ResolveArtifactTask.java
+++ src/main/java/org/apache/maven/plugin/antrun/ResolveArtifactTask.java
@@ -4,6 +4,8 @@ import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
 
 /**
  * Ant task that resolves an artifact through Maven.
@@ -35,12 +37,22 @@ public class ResolveArtifactTask extends Task {
     }
 
     public void execute() throws BuildException {
-        log("Log messages this way", Project.MSG_INFO);
+        log("Starting execute", Project.MSG_DEBUG);
+        try {
+            ArtifactResolverWrapper w = ArtifactResolverWrapper.get();
 
-        ArtifactResolverWrapper w = ArtifactResolverWrapper.get();
-
-        // TODO: do the resolution
-        Artifact a = w.getFactory().createArtifactWithClassifier(groupId, artifactId, version, null, classifier);
-        w.getResolver().resolve();
+            // TODO: do the resolution
+            Artifact a = w.getFactory().createArtifactWithClassifier(groupId, artifactId, version, null, classifier);
+            w.getResolver().resolve(a, w.getRemoteRepositories(), w.getLocalRepository());
+        } catch (ArtifactResolutionException ex) {
+            // TODO: decide if the exception should be thrown
+            log("Problem resolving artifact: "+ex.getMessage(), Project.MSG_ERR);
+            throw new BuildException(ex);
+        } catch (ArtifactNotFoundException ex) {
+            // TODO: decide if the exception should be thrown
+            log("Problem resolving artifact: "+ex.getMessage(), Project.MSG_ERR);
+            throw new BuildException(ex);
+        }
+        log("Exiting execute", Project.MSG_DEBUG);
     }
 }
