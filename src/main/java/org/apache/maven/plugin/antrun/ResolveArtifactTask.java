c9974ec3e8d483ba44060c2fda809fad27d0fc73
Paul Sterk <psterk@java.net>
2007-10-10 18:54:52 +0000

    Simplified exception handling.
    svn path=/trunk/; revision=22

--- src/main/java/org/apache/maven/plugin/antrun/ResolveArtifactTask.java
+++ src/main/java/org/apache/maven/plugin/antrun/ResolveArtifactTask.java
@@ -4,8 +4,6 @@ import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
-import org.apache.maven.artifact.resolver.ArtifactResolutionException;
 
 /**
  * Ant task that resolves an artifact through Maven.
@@ -40,16 +38,9 @@ public class ResolveArtifactTask extends Task {
         log("Starting execute", Project.MSG_DEBUG);
         try {
             ArtifactResolverWrapper w = ArtifactResolverWrapper.get();
-
-            // TODO: do the resolution
             Artifact a = w.getFactory().createArtifactWithClassifier(groupId, artifactId, version, null, classifier);
             w.getResolver().resolve(a, w.getRemoteRepositories(), w.getLocalRepository());
-        } catch (ArtifactResolutionException ex) {
-            // TODO: decide if the exception should be thrown
-            log("Problem resolving artifact: "+ex.getMessage(), Project.MSG_ERR);
-            throw new BuildException(ex);
-        } catch (ArtifactNotFoundException ex) {
-            // TODO: decide if the exception should be thrown
+        } catch (Throwable ex) {
             log("Problem resolving artifact: "+ex.getMessage(), Project.MSG_ERR);
             throw new BuildException(ex);
         }
