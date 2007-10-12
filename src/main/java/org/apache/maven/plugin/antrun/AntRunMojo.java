268883f4c93e8aee79887169478706631ef43fd1
Paul Sterk <psterk@java.net>
2007-10-12 22:19:15 +0000

    Configured the package phase to use org.jvnet.maven-antrun-extended-plugin:maven-antrun-extended-plugin. This is used for <role-hint> 'zip'.  Since this is configured in the components.xml, it is not needed in AntRunMojo.java.
    svn path=/trunk/; revision=30

--- src/main/java/org/apache/maven/plugin/antrun/AntRunMojo.java
+++ src/main/java/org/apache/maven/plugin/antrun/AntRunMojo.java
@@ -41,7 +41,6 @@ import org.apache.tools.ant.Target;
  * @version $Id: AntRunMojo.java 557316 2007-07-18 16:29:17Z kenney $
  * @configurator override
  * @goal run
- * @phase package
  * @requiresDependencyResolution test
  */
 // TODO: phase package
