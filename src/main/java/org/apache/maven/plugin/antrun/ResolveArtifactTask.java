7fe7de7aa938cc24032275ae0bf2e3e03981086c
Kohsuke Kawaguchi <kohsuke@java.net>
2007-10-19 21:29:15 +0000

    added the tofile/todir just like the copy task, so that the caller can put the artifact in a desired place.
    svn path=/trunk/; revision=32

--- src/main/java/org/apache/maven/plugin/antrun/ResolveArtifactTask.java
+++ src/main/java/org/apache/maven/plugin/antrun/ResolveArtifactTask.java
@@ -1,10 +1,12 @@
 package org.apache.maven.plugin.antrun;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
-import org.apache.maven.artifact.Artifact;
-import org.apache.tools.ant.taskdefs.Property;
+import org.apache.tools.ant.taskdefs.Copy;
+
+import java.io.File;
 
 /**
  * Ant task that resolves an artifact through Maven.
@@ -18,6 +20,8 @@ import org.apache.tools.ant.taskdefs.Property;
 public class ResolveArtifactTask extends Task {
 
     private String property,groupId,artifactId,version,type="jar",classifier;
+
+    private File tofile,todir;
 
     public void setProperty(String property) {
         this.property = property;
@@ -43,6 +47,22 @@ public class ResolveArtifactTask extends Task {
         this.classifier = classifier;
     }
 
+    /**
+     * The file name to copy the artifact to.
+     * Optional. The name is 
+     */
+    public void setTofile(File target) {
+        this.tofile = target;
+    }
+
+    /**
+     * The file name to copy the artifact to.
+     * Optional.
+     */
+    public void setTodir(File target) {
+        this.tofile = target;
+    }
+
     public void execute() throws BuildException {
         log("Starting execute", Project.MSG_DEBUG);
         try {
@@ -52,6 +72,24 @@ public class ResolveArtifactTask extends Task {
             // Property attribute is optional. Check for null value
             if (property != null) {                
                 getProject().setProperty(property, a.getFile().getAbsolutePath());
+            }
+
+            if(tofile!=null) {
+                log("Copying "+a.getFile()+" to "+tofile);
+                Copy cp = new Copy();
+                cp.setProject(getProject());
+                cp.setFile(a.getFile());
+                cp.setTofile(tofile);
+                cp.execute();
+            }
+
+            if(todir!=null) {
+                log("Copying "+a.getFile()+" to "+todir);
+                Copy cp = new Copy();
+                cp.setProject(getProject());
+                cp.setFile(a.getFile());
+                cp.setTodir(todir);
+                cp.execute();
             }
         } catch (Throwable ex) {
             log("Problem resolving artifact: "+ex.getMessage(), Project.MSG_ERR);
