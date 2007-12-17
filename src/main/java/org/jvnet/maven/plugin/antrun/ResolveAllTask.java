a517fc5c40ab5401f84234cd8be2963e8b30d662
Paul Sterk <psterk@java.net>
2007-12-17 23:46:49 +0000

    Added code to ignore duplicate files found by the <resolveAll/> task. Rev'ed the version number to '11'.
    svn path=/tags/maven-antrun-extended-plugin-1.9/; revision=205

--- src/main/java/org/jvnet/maven/plugin/antrun/ResolveAllTask.java
+++ src/main/java/org/jvnet/maven/plugin/antrun/ResolveAllTask.java
@@ -12,6 +12,8 @@ import org.apache.tools.ant.types.Path;
 import java.io.File;
 import java.io.IOException;
 import java.util.Set;
+import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -26,6 +28,8 @@ public class ResolveAllTask extends ConditionBase {
     private String property;
 
     private String groupId,artifactId,version,type="jar",classifier;
+
+    private boolean ignoreDuplicates = false;
 
     public void setProperty(String property) {
         this.property = property;
@@ -59,7 +63,11 @@ public class ResolveAllTask extends ConditionBase {
     public void setPathId(String pathId) {
         this.pathId = pathId;
     }
-    
+
+    public void ignoreDuplicates(boolean ignoreDuplicates) {
+        this.ignoreDuplicates = ignoreDuplicates;
+    }
+
     public void execute() throws BuildException {  
         log("Starting ResolveAllTasks.execute ", Project.MSG_DEBUG);
         try {
@@ -87,6 +95,7 @@ public class ResolveAllTask extends ConditionBase {
             // For each artifact, get the pom file and see if the value for
             // <packaging/> child element matches the Condition
             Path path = new Path(getProject());
+            Collection duplicateFileLookup = new HashSet();
             for (Artifact artifact : artifacts) {
                 CURRENT_ARTIFACT.set(artifact);
                 if (countConditions() > 1) {
@@ -101,8 +110,7 @@ public class ResolveAllTask extends ConditionBase {
                     if (!c.eval())
                         continue;   // rejected
                 }
-                
-                handleArtifact(artifact, path);
+                handleArtifact(artifact, path, duplicateFileLookup);
             }
             if (pathId != null) {
                 getProject().addReference(pathId, path);
@@ -112,18 +120,91 @@ public class ResolveAllTask extends ConditionBase {
         }
         log("Exiting ResolveAllTasks.execute ", Project.MSG_DEBUG);
     }
-    
-    private void handleArtifact(Artifact artifact, Path path) throws IOException {
-        log("Starting ResolveAllTasks.handleArtifact "+todir, Project.MSG_DEBUG);
-        File artifactFile = artifact.getFile();
-        path.createPathElement().setLocation(artifactFile);
-        // If todir is not null, copy each artifact to the todir directory
-        if (todir != null) {
-            File outFile = new File(todir,artifactFile.getName());
-            FileUtils.copyFile(artifactFile,outFile);
+
+    private void handleArtifact(Artifact artifact, Path path, Collection duplicateFileLookup) throws IOException {
+        log("Starting ResolveAllTasks.handleArtifact ", Project.MSG_DEBUG);
+        // Check to see if the jar file already exists in the toDir directory tree
+        if (! ignoreDuplicate(artifact, todir, duplicateFileLookup)) {
+            File artifactFile = artifact.getFile();
+            path.createPathElement().setLocation(artifactFile);
+            // If todir is not null, copy each artifact to the todir directory
+            if (todir != null) {
+                File outFile = new File(todir,artifactFile.getName());
+                FileUtils.copyFile(artifactFile,outFile);
+            }
         }
-        log("Exiting ResolveAllTasks.handleArtifact "+todir, Project.MSG_DEBUG);
+        log("Exiting ResolveAllTasks.handleArtifact ", Project.MSG_DEBUG);
     }
 
+    private boolean ignoreDuplicate(Artifact artifact, File dir, Collection duplicateFileLookup) {
+        log("Entering ResolveAllTasks.ignoreDuplicate", Project.MSG_DEBUG);
+        boolean ignoreDuplicate = false;
+        if (ignoreDuplicates) {
+            ignoreDuplicate = true;
+        } else {
+            log("artifact id: "+artifact.getId(), Project.MSG_DEBUG);
+            String artifactName = getFullArtifactName(artifact);
+            log("Check for duplicates using this artifactName: "+artifactName, Project.MSG_DEBUG);
+            if (duplicateFileLookup.isEmpty()) {
+                // populate the Map with files that currently exist in the path specified by
+                // this.todir
+                createDuplicateFileLookup(dir, duplicateFileLookup);
+            }
+            if (duplicateFileLookup.contains(artifactName)) {
+                log("Found duplicate: " + artifactName, Project.MSG_DEBUG);
+                ignoreDuplicate = true;
+            }
+        }
+        log("Exiting ResolveAllTasks.ignoreDuplicate", Project.MSG_DEBUG);
+        return ignoreDuplicate;
+    }
+
+    private String getFullArtifactName(Artifact artifact) {
+        StringBuffer sb = new StringBuffer(artifact.getArtifactId());
+        sb.append("-").append(artifact.getVersion());
+        sb.append(".").append(artifact.getType());
+        return sb.toString();
+    }
+    
+    private void createDuplicateFileLookup(File dir, Collection<String> duplicateFileLookup) {
+        log("Entering ResolveAllTasks.createDuplicateFileLookup", Project.MSG_DEBUG);
+        String directoryName = dir.getAbsolutePath();
+        // Check to see if this directory has already been processed
+        if (duplicateFileLookup.contains(directoryName)) {
+            // do not process
+            log("Directory already processed", Project.MSG_DEBUG);
+            log("Exiting ResolveAllTasks.createDuplicateFileLookup", Project.MSG_DEBUG);
+            return;
+        }
+        log ("Traversing this directory: "+directoryName, Project.MSG_DEBUG);
+        // Record the directory name to avoid recursive traversals
+        duplicateFileLookup.add(directoryName);
+        log("Directory name is: "+directoryName, Project.MSG_DEBUG);
+        File[] files = dir.listFiles();
+        int numFiles = files.length;
+        log ("Number of files: "+numFiles, Project.MSG_DEBUG);
+        for (int i = 0; i < numFiles; i++) {
+            log ("index i: "+i, Project.MSG_DEBUG);
+            File file = files[i];
+            String fileName = file.getName();
+            if (file.isDirectory()) {
+                createDuplicateFileLookup(file, duplicateFileLookup);
+            } else {                
+                log ("Adding this file name: "+fileName, Project.MSG_DEBUG);
+                duplicateFileLookup.add(fileName);
+            }
+        }
+        File parent = dir.getParentFile();
+        String parentDirPath = parent.getAbsolutePath();
+        log ("parent dir path: "+parentDirPath, Project.MSG_DEBUG);
+        // TODO: look up the target directory using Maven API
+        if (parent != null && ! parentDirPath.endsWith("target")) {
+            // Do not traverse parent above maven 'target' directory
+            createDuplicateFileLookup(parent, duplicateFileLookup);
+        }
+        log("duplicateFileLookup size: "+duplicateFileLookup.size(), Project.MSG_DEBUG);
+        log("Exiting ResolveAllTasks.createDuplicateFileLookup", Project.MSG_DEBUG);
+    }
+    
     public static final ThreadLocal<Artifact> CURRENT_ARTIFACT = new ThreadLocal<Artifact>();
 }
