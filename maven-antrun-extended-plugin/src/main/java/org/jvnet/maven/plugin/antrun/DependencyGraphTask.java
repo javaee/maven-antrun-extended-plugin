a1664913da83f4a1fd0b58c89729b7897c5b6ba2
Kohsuke Kawaguchi <kohsuke@java.net>
2008-03-18 03:10:11 +0000

    adding support for list-based filtering, where the filtering works on a list of artifacts, instead of graph.
    svn path=/trunk/; revision=383

--- maven-antrun-extended-plugin/src/main/java/org/jvnet/maven/plugin/antrun/DependencyGraphTask.java
+++ maven-antrun-extended-plugin/src/main/java/org/jvnet/maven/plugin/antrun/DependencyGraphTask.java
@@ -1,15 +1,15 @@
 package org.jvnet.maven.plugin.antrun;
 
 import org.apache.maven.artifact.resolver.AbstractArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
 import org.apache.maven.project.ProjectBuildingException;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * Base class for tasks that work with a dependency graph defined as a nested structure.
@@ -18,8 +18,8 @@ import java.util.List;
  * @author Paul Sterk
  */
 public abstract class DependencyGraphTask extends Task {
-    protected String groupId,artifactId,version,type="jar",classifier;
-    protected final List<GraphFilter> children = new ArrayList<GraphFilter>();
+    private String groupId,artifactId,version,type="jar",classifier;
+    private String baseGraph;
 
     public void setGroupId(String groupId) {
         this.groupId = groupId;
@@ -41,44 +41,26 @@ public abstract class DependencyGraphTask extends Task {
         this.classifier = classifier;
     }
 
-    /**
-     * Adds a {@link GraphFilter} child. Ant will invoke this for each child element given in build script.
-     */
-    public void add(GraphFilter child) {
-        children.add(child);
+    public void setBaseGraph(String id) {
+        this.baseGraph = id;
     }
 
-    protected DependencyGraph buildGraph() {
+    /**
+     * Transforms a graph by applying the given filter to the "source graph",
+     * which is determined by the various parameter to this task.
+     */
+    protected DependencyGraph buildGraph(GraphFilter filter) {
         try {
-            MavenComponentBag w = MavenComponentBag.get();
+            DependencyGraph g = buildSourceGraph();
+            if(filter==null)    return g;
 
-            DependencyGraph g;
-
-            if(groupId==null && artifactId==null && version==null) {
-                // if no clue is given whatsoever, use all the project dependencies
-                g = new DependencyGraph(w.project.getArtifact());
-            } else {
-                // otherwise pick up dependencies from the specified artifact
-                g = new DependencyGraph(w.createArtifactWithClassifier(groupId,artifactId,version,type,classifier));
-                log("artifactId "+artifactId,  Project.MSG_DEBUG);
-            }
-
-            g = new DependencyExclusionFilter().filter(g);
-
-            log("Graph="+g,Project.MSG_DEBUG);
-
-            if(children.size()>1)
-                throw new BuildException("Too many filters are given");
-
-            if(!children.isEmpty()) {
-                // apply transformation to g
-                final DependencyGraph old = GraphFilter.CURRENT_INPUT.get();
-                GraphFilter.CURRENT_INPUT.set(g);
-                try {
-                    g = children.get(0).process();
-                } finally {
-                    GraphFilter.CURRENT_INPUT.set(old);
-                }
+            // apply transformation to g
+            final DependencyGraph old = GraphFilter.CURRENT_INPUT.get();
+            GraphFilter.CURRENT_INPUT.set(g);
+            try {
+                g = filter.process();
+            } finally {
+                GraphFilter.CURRENT_INPUT.set(old);
             }
 
             Collection<DependencyGraph.Node> nodes = g.getAllNodes();
@@ -93,5 +75,33 @@ public abstract class DependencyGraphTask extends Task {
         } catch (ProjectBuildingException e) {
             throw new BuildException(e);
         }
+    }
+
+    /**
+     * Computes the source grpah.
+     */
+    private DependencyGraph buildSourceGraph() throws ProjectBuildingException, ArtifactResolutionException, ArtifactNotFoundException, IOException {
+        MavenComponentBag w = MavenComponentBag.get();
+
+        DependencyGraph g;
+
+        if(baseGraph!=null) {
+            g = (DependencyGraph)getProject().getReference(baseGraph);
+            if(g==null)
+                throw new BuildException("There's no graph with id="+baseGraph);
+        } else
+        if(groupId==null && artifactId==null && version==null) {
+            // if no clue is given whatsoever, use all the project dependencies
+            g = new DependencyGraph(w.project.getArtifact());
+        } else {
+            // otherwise pick up dependencies from the specified artifact
+            g = new DependencyGraph(w.createArtifactWithClassifier(groupId,artifactId,version,type,classifier));
+            log("artifactId "+artifactId,  Project.MSG_DEBUG);
+        }
+
+        g = new DependencyExclusionFilter().filter(g);
+
+        log("Graph="+g,Project.MSG_DEBUG);
+        return g;
     }
 }
